3521c8a3e1d0d5870a807d6e5f76e2008bf48716
Douglas Borges Andrade <dba12@hotmail.com>
2025-09-01 15:32:15 -0300

    feat: add management and structured logger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"company-service/internal/config"
+	"company-service/pkg/logger"
+	"log"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+
+	// Carregando configurações
+	cfg, err := config.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("Falha ao carregar as configurações: %v", err)
+	}
+
+	logger, err := logger.New(cfg.LogLevel)
+	if err != nil {
+		log.Fatal("Falha ao iniciar o logger")
+	}
+	defer logger.Sync()
+
+	logger.Info("Configurações carregadas com sucesso")
+	logger.Debug("Detalhes de Configuração:", zap.Any("config", cfg))
+}
---
+++ pkg/logger/logger.go
@@ -0,0 +1,55 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func New(logLevel string) (*zap.Logger, error) {
+	// Conversão de string level para zap.AtomicLevel
+
+	var level zap.AtomicLevel
+	switch logLevel {
+	case "debug":
+		level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	case "info":
+		level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	case "warn":
+		level = zap.NewAtomicLevelAt(zap.WarnLevel)
+	case "error":
+		level = zap.NewAtomicLevelAt(zap.ErrorLevel)
+	default:
+		level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	}
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "timestamp",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	config := zap.Config{
+		Level:             level,
+		Development:       false,
+		DisableCaller:     false,
+		DisableStacktrace: true,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding:         "json",
+		EncoderConfig:    encoderConfig,
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	return config.Build()
+}
